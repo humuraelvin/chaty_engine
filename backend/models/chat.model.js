8ba4dcb879677ce7e3225d95accd65286265efa0
humuraelvin <humura53@gmail.com>
2023-07-16 00:00:00 +0000

    changes added

--- backend/models/chat.model.js
+++ backend/models/chat.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const chatModel = mongoose.model(
     {
         chatName: { type: String, trim:true },
-        isGroupChat: { type: Boolean, default: false }
+        isGroupChat: { type: Boolean, default: false },
+        users: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }]
     }
 )
