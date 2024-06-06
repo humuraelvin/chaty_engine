e4bb49da9972ea0f0a762d165d456e6d216d82d6
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 13:32:11 +0200

    changes made

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -9,6 +9,8 @@ const errorHandler = (err, req, res, next) => {
     res.status(statusCode);
     res.json({
         message: err.message,
-        stack: 
-    })
-}
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+};
+
+module.exports = { notFound, errorHandler };
