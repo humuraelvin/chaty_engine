4b9c1fb19805a0992f7b45514b05c08627c3f554
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 20:24:17 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,8 @@
 const asyncHandler = require('express-async-handler');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user.model')
+const User = require('../models/user.model');
+const generateToken = require('../utils/generateToken');
+
+
+const allUsers = asyncHandler(async(req, res) => {
+    const keyWord = req.query.search;
+})
