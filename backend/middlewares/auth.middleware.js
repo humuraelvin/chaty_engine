4686aa81998595b484682e0a1278a3d5444b480f
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 20:16:11 +0200

    changes made

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -2,8 +2,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 const asyncHandler = require('express-async-handler');
 
-const protect = asyncHandler ((req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
 
-    
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+
+        try {
+            token = req.headers.authorization.split("")[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+
+        } catch (error) {
+
+        }
+
+    }
 
 })
