bee9e20e572b33b9af0ad41742bda6dba84a97ec
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 20:09:15 +0200

    changes made

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+const asyncHandler = require('express-async-handler');
+
+const protect = asyncHandler ((req, res, next) => {
+
+    
+
+})
