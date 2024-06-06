d12735e25d3193a45ad4a2ac0c6d335a835dbe79
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 22:13:03 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,6 +3,16 @@ const User = require('../models/user.model');
 const generateToken = require('../utils/generateToken');
 
 
-const allUsers = asyncHandler(async(req, res) => {
-    const keyWord = req.query.search;
-})
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search
+        ? {
+            $or: [
+                { name: { $regex: req.query.search, $options: "i" } },
+                { email: { $regex: req.query.search, $options: "i" } },
+            ],
+        }
+        : {};
+
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
+});
