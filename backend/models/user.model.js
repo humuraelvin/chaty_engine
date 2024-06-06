03e3c64bd2747eb4d20db70cfaf5805f76ebf934
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 11:00:24 +0200

    changes made

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,22 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
-    name: { type: "String", required: true },
-    email: { type: "String", unique: true, required: true },
-    password: { type: "String", required: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
     pic: {
-        type: "String",
+        type: String,
         required: true,
-        default:
-            "https://icon-library.com/images/anonymous-avatar-icon/anonymous-icon-avatar-25.jpg",
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-icon-avatar-25.jpg",
     },
     isAdmin: {
         type: Boolean,
         required: true,
         default: false
-    },
+    }
 },
-
-    { timeStamps:true }
-
-)
+    { timestamps: true }
+);
