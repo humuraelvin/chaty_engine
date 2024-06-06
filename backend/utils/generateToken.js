2f456604c6b0a155aba729b39c54bac5aa1eba47
HUMURA ELVIN <humura53@gmail.com>
2024-06-06 12:04:05 +0200

    changes made

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, )
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1d",
+    });
+};
+
+module.exports = generateToken;
