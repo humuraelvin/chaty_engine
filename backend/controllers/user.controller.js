75f957a44d43e15d0c368741589b07d0bea06499
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 11:16:53 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -27,7 +27,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Please input all fields");
     };
 
-    const userExists = await User.find({ email });
+    const userExists = await User.findOne({ email });
 
     if (userExists) {
         res.status(400);
@@ -55,4 +55,30 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("User registration failed");
     }
 
-})
+});
+
+
+const authUser = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            pic: user.pic,
+            token: generateToken(user._id),
+        });
+
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
+
+});
+
