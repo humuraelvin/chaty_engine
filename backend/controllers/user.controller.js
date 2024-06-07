c2a285a4ef0c76e7f733270ae18519adc7033483
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 11:07:45 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,3 +16,22 @@ const allUsers = asyncHandler(async (req, res) => {
     const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
     res.send(users);
 });
+
+
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password, pic } = req.body;
+
+    if (!name || !email || !password || !pic) {
+        res.status(400);
+        throw new Error("Please input all fields");
+    };
+
+    const userExists = await User.find({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User with provided email already exists");
+    }
+
+})
