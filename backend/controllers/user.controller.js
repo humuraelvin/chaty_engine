188519f3266cfee953fce4ef860d91ece58c59c7
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 11:11:57 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -34,4 +34,25 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("User with provided email already exists");
     }
 
+    const user = await User.create({
+        name,
+        email,
+        password,
+        pic,
+    });
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            pic: user.pic,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error("User registration failed");
+    }
+
 })
