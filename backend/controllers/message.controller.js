4786acfc55b2250ed952058f2f8fe96c6fad21e7
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 15:32:11 +0200

    changes made

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -28,7 +28,61 @@ const accessChat = asyncHandler(async (req, res) => {
     });
 
     if (isChat.lenght > 0) {
+        res.send(isChat[0]);
+    } else {
+        var chatData = {
+            chatName: "sender",
+            isGroupChat: false,
+            users: [req.user._id, userId],
+        };
 
+        try {
+            const createdChat = await Chat.create(chatData);
+            const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
+                "users",
+                "-password"
+            );
+            res.status(200).json(FullChat);
+        } catch (error) {
+            res.status(400);
+            throw new Error(error.message);
+        }
+
+    }
+
+})
+
+const fetchChats = asyncHandler(async (req, res) => {
+    try {
+        Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({ updatedAt: -1 })
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: "latestMessage",
+                    select: "name pic email",
+                });
+                res.status(200).send(results)
+            })
+
+    } catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+
+    }
+});
+
+const createGroupChat = asyncHandler(async (req, res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please Fill All the fields  " })
+    }
+
+    var users = JSON.parse(req.body.users);
+
+    if (users.lenght < 2) {
+        return res.status(400).send({message: "Group Chat can be formed by more than 2 people please"})
     }
 
 })
