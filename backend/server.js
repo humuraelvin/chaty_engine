766a03449cedeec92410b8a5c948575ac3997294
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 17:19:53 +0200

    changes made

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,27 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const dbconnection = require('./utils/dbconnection')
+const userRoutes = require('./routes/user.routes');
+const chatRoutes = require('./routes/chat.routes')
+const messageRoutes = require('./routes/message.routes')
+const { notFound, errorHandler } = require('./middlewares/error.middleware')
+const path = require('path');
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended:true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+
+app.use(notFound);
+app.use(errorHandler)
 
 
 //db connection
