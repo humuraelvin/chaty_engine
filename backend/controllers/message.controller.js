fa858a62380dfcc2a672e72e4701fe84102a4fff
HUMURA ELVIN <humura53@gmail.com>
2024-06-07 13:36:38 +0200

    changes made

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,3 +1,23 @@
 const asyncHandler = require('express-async-handler');
 const Chat = require('../models/chat.model');
 const User = require('../models/user.model');
+
+
+const accessChat = asyncHandler(async (req, res) => {
+    const { userId } = req.body;
+
+    if (userId) {
+        console.log("Userid param not sent with request");
+        return res.sendStatus(400);
+    }
+
+    var isChat = await Chat.find({
+        isGroupChat: false,
+        $and: [
+            { $users: { $elemMatch: { $eq: req.user._id } } },
+            { $users: { $elemMatch: { $eq: userId } } },
+        ],
+
+    })
+
+})
