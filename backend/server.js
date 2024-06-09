d40ab7b2eb7fdb53ee1a17ce8e190e4c180d2b30
HUMURA ELVIN <humura53@gmail.com>
2024-06-09 10:53:54 +0200

    changes made

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,31 @@ io.on("connection", (socket) => {
     });
 
     socket.on("join chat", (room) => {
-        
-    })
+        socket.join(room)
+        console.log("User joined room" + room);
+    });
 
-})
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if (!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach((user) => {
+
+            if (user._id == newMessageReceived.sender._id) return;
+
+            socket.in(user._id).emit("message recieved", newMessageReceived);
+
+        });
+
+    });
+
+    socket.off("setup", () => {
+        console.log("User disconnected");
+        socket.leave(userData._id);
+    });
+
+});
