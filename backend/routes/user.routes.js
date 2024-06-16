ea13abfd012a3ddab6a01691224567648ae383c9
humuraelvin <humura53@gmail.com>
2024-06-16 13:54:23 +0200

    changes made

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 const { registerUser, authUser, allUsers } = require('../controllers/user.controller');
-const { protectRoutes } = require('../middlewares/auth.middleware');
+const { protectRoute } = require('../middlewares/auth.middleware'); // Corrected import
 
 const router = express.Router();
 
-router.route("/").get(protectRoutes, allUsers);
+router.route("/").get(protectRoute, allUsers); // Consistent naming
 router.route("/").post(registerUser);
 router.post("/login", authUser);
 
-module.exports = router
+module.exports = router;
