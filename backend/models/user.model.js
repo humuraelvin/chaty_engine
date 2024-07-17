8d92eac9fabb533a8767f43887ad0fc932a8e18a
humuraelvin <humura53@gmail.com>
2024-07-17 00:00:00 +0000

    changes added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
     {
-        name: {type: String, required: true}
+        name: {type: String, required: true},
+        email: {type: String, required: true},
+        
     }
 )
