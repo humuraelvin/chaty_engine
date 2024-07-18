ef76819e35672a8df34ad1d7090454dca36a324c
humuraelvin <humura53@gmail.com>
2024-07-18 20:50:58 +0200

    changes made

--- backend/utils/dbconnection.js
+++ backend/utils/dbconnection.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const dbconnection = async () => {
     try {
         
-        await mongoose.connect(process.env.M)
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDb successfully");
 
     } catch (error) {
-        
+        console.log("Error connecting to mongodb", error);
     }
 }
