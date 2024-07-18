a6155c7a84c7dd38c0371275d3c74f7bd1df061b
humuraelvin <humura53@gmail.com>
2024-07-18 16:39:27 +0200

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Route } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import ChatPage from './Pages/ChatPage'
 
 function App() { 
 
   return (
     <div className='App'>
-      
+      <Route path='/' exact Component={<HomePage />}/>
+      <Route path='/chats' exact Component={<ChatPage />}/>
     </div>
   )
 }
