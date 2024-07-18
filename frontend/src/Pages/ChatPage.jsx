936ab171396987de9981cda9d47a1b1a054268f8
humuraelvin <humura53@gmail.com>
2024-07-18 21:05:53 +0200

    changes made

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const ChatPage = () => {
+
+    const fetchChats = async () => {
+        const { data } = await axios.get('/api/chat');
+
+        console.log(data);
+
+    }
+
   return (
     <div>
       Chats
