5f070ab05ed6ccc8d77b38cc9382843ecc13a42a
humuraelvin <humura53@gmail.com>
2024-07-19 00:00:57 +0200

    changes made

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,4 +7,10 @@ const userSchema = mongoose.Schema(
         password: {type: String, required: true},
         pic: { type:String, required: true, default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg' },
     }
+    ,
+    {
+        timestamps: true,
+    }
 )
+
+const User = mongoose.model("User", userSchema)
