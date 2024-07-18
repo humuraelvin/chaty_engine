5de39fcc169a4a94a709bbc5cd00ff8d0992c067
humuraelvin <humura53@gmail.com>
2024-07-18 20:49:33 +0200

    changes made

--- backend/utils/dbconnection.js
+++ backend/utils/dbconnection.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+
+const dbconnection = async () => {
+    try {
+        
+        await mongoose.connect(process.env.M)
+
+    } catch (error) {
+        
+    }
+}
