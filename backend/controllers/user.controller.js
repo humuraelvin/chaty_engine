9c376a3208b06981090a1f0bfab090e14365ce54
humuraelvin <humura53@gmail.com>
2024-07-19 15:28:07 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user.model.js')
 const generateToken = require('../utils/generateToken.js')
+
+
+const allUsers = asyncHandler(async(req, res) => 
+
+    {
+        
+    }
+
+)
