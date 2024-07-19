74eff8e3fb246fee0300cb5891cc9aebe681d3a3
humuraelvin <humura53@gmail.com>
2024-07-19 15:35:47 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -9,7 +9,8 @@ const allUsers = asyncHandler(async(req, res) =>
         const keyword = req.query.search
         ?{
             $or:[
-                {}
+                {name:{$regex:req.query.search, $options:"i"}},
+                {email:{$regex:req.query.search, $options:"i"}}
             ]
         }
     }
