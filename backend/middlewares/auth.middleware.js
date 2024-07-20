549c2d42fc9fc445c669505725924fec2b355a85
humuraelvin <humura53@gmail.com>
2024-07-20 22:54:32 +0200

    changes made

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,5 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user.model.js')
+const asyncHandler = require('express-async-handler')
+
+const protectRoute = asyncHandler(async(req, res, nextg))
