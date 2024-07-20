085d11c716fb2fbc9088e43681c5a50677b92f2e
humuraelvin <humura53@gmail.com>
2024-07-20 22:32:37 +0200

    changes made

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -21,5 +21,18 @@ const allUsers = asyncHandler(async(req, res) =>
 })
 
 const registerUser = asyncHandler(async(req, res) => {
-    const
+    const { name, email, password, pic } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error("Please Enter all the fields")
+    }
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+
 })
