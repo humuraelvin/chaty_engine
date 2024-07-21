8f75ea4b600d7464f165fc1f277859ea84896c97
humuraelvin <humura53@gmail.com>
2024-07-21 13:05:21 +0200

    changes made

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -25,5 +25,11 @@ const sendMessage = asyncHandler(async(req, res) =>
 
     {
         const { content, chatId } = req.body;
+
+        if (!content || !chatId ) {
+            console.log("Invalid data passed into request");
+            return res.sendStatus(400)
+        }
+
     }
 )
