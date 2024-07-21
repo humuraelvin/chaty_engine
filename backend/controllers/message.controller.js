c98b4039df74d42244e8b0c9711a61e34825fe86
humuraelvin <humura53@gmail.com>
2024-07-21 13:03:04 +0200

    changes made

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -2,3 +2,19 @@ const asyncHandler = require('express-async-handler')
 const Message = require('../models/message.model.js')
 const User = require('../models/user.model.js')
 const Chat = require('../models/chat.model.js')
+
+
+const allMessages = asyncHandler(async (req, res) =>
+
+    {
+    try {
+        const messages = await Message.find({ chat: req.params.chatId })
+            .populate("sender", "name pic email")
+            .populate("chat")
+
+          res.json(messages)
+    } catch (error) {
+        
+    }
+    }
+)
