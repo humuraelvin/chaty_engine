60b1edccacd437a219624de960afa288dc1d7ce6
humuraelvin <humura53@gmail.com>
2024-07-21 13:16:16 +0200

    changes made

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -2,3 +2,24 @@ const asyncHandler = require('express-async-handler')
 const Chat = require('../models/chat.model.js')
 const User = require('../models/user.model.js')
 
+
+const accessChat = asyncHandler(async( req, res) => 
+
+    {
+        const { userId } = req.body;
+
+        if (!userId) {
+            console.log("UserId param not sent with request");
+            return res.sendStatus(400);
+
+            var isChat = await Chat.find({
+                isGroupChat:false,
+                $and: [
+                    { users: { $elemMatch: { $eq: req.user._id } } },
+                    { users: { $elemMatch: { $eq: userId } } }
+                ]
+            })
+        }
+
+    }
+)
