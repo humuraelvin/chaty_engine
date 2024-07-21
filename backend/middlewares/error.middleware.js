0ad19bb97e45b56a1a3d1629e7385129fd7342bd
humuraelvin <humura53@gmail.com>
2024-07-21 12:56:04 +0200

    changes made

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -2,5 +2,10 @@ const notFound = (req, res, next) => {
    
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404)
+    next(error)
+}
 
+
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
 }
