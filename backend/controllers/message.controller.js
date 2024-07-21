5b085a7958ab2020cf0d41455c28b85e837e41ea
humuraelvin <humura53@gmail.com>
2024-07-21 13:00:44 +0200

    changes made

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,2 +1,4 @@
 const asyncHandler = require('express-async-handler')
 const Message = require('../models/message.model.js')
+const User = require('../models/user.model.js')
+const Chat = require('../models/chat.model.js')
